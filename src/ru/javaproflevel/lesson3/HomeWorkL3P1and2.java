6d9e34219700274c1393951632150c1ad4530aa2
BoldyrevMichael <mmmsnddd@icloud.com>
2017-09-02 14:54:32 +0300

    Homework lesson3 second part

--- src/ru/javaproflevel/lesson3/HomeWorkL3P1and2.java
+++ src/ru/javaproflevel/lesson3/HomeWorkL3P1and2.java
@@ -1,7 +1,11 @@
 package ru.javaproflevel.lesson3;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.SequenceInputStream;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class HomeWorkL3P1and2 {
 
@@ -10,7 +14,23 @@ public class HomeWorkL3P1and2 {
         FileInputStream fis = new FileInputStream("file50byte.txt");
         byte[] bytes = new byte[fis.available()];
         fis.read(bytes);
+        fis.close();
         System.out.println(Arrays.toString(bytes));
 
+        ArrayList<FileInputStream> arrayList = new ArrayList<>();
+        arrayList.add(new FileInputStream("file100byte1.txt"));
+        arrayList.add(new FileInputStream("file100byte2.txt"));
+        arrayList.add(new FileInputStream("file100byte3.txt"));
+        arrayList.add(new FileInputStream("file100byte4.txt"));
+        arrayList.add(new FileInputStream("file100byte5.txt"));
+
+        SequenceInputStream sin = new SequenceInputStream(Collections.enumeration(arrayList));
+        FileOutputStream out = new FileOutputStream("file500byte.txt");
+        int r;
+        while ((r = sin.read()) != -1) {
+            out.write(r);
+        }
+        sin.close();
+        out.close();
     }
 }
