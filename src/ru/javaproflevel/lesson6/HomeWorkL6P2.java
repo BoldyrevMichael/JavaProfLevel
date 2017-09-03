e2a11aa4ef08644ba5364d5be6d052c24b3073e0
BoldyrevMichael <mmmsnddd@icloud.com>
2017-09-03 23:01:19 +0300

    Homework lesson6 second part

--- src/ru/javaproflevel/lesson6/HomeWorkL6P2.java
+++ src/ru/javaproflevel/lesson6/HomeWorkL6P2.java
@@ -1,4 +1,15 @@
 package ru.javaproflevel.lesson6;
 
 public class HomeWorkL6P2 {
+
+    public boolean checkArrayFor1and4(int[] array) {
+        boolean f = true;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != 1 && array[i] != 4) {
+                f = false;
+                break;
+            }
+        }
+        return f;
+    }
 }
--- test/java/HomeWorkL6P2Test.java
+++ test/java/HomeWorkL6P2Test.java
@@ -1,2 +1,47 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import ru.javaproflevel.lesson6.HomeWorkL6P2;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(value = Parameterized.class)
 public class HomeWorkL6P2Test {
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(
+                new Object[][]{
+
+                        {new boolean[]{true}, new int[]{1, 1, 1, 1, 1, 1, 1}},
+                        {new boolean[]{true}, new int[]{4, 4, 4, 4, 4, 4, 4}},
+                        {new boolean[]{true}, new int[]{1, 4, 1, 4, 1, 4, 1}},
+                        {new boolean[]{false}, new int[]{0, 2, 3, 5, 6, 7, 8}},
+                        {new boolean[]{false}, new int[]{4, 1, 4, 1, 4, 1, 0}}
+                }
+        );
+    }
+
+    private int[] arrayIn;
+    private boolean[] arrayExp;
+
+    public HomeWorkL6P2Test(boolean[] arrayExp, int[] arrayIn) {
+        this.arrayIn = arrayIn;
+        this.arrayExp = arrayExp;
+    }
+
+    HomeWorkL6P2 homeWorkL6P2;
+
+    @Before
+    public void init() {
+        homeWorkL6P2 = new HomeWorkL6P2();
+    }
+
+    @Test
+    public void massTestCheckArray() {
+        Assert.assertArrayEquals(arrayExp, new boolean[]{homeWorkL6P2.checkArrayFor1and4(arrayIn)});
+    }
 }
