e79a76110353c8272555aaa966959567d474684e
BoldyrevMichael <mmmsnddd@icloud.com>
2017-09-04 19:55:27 +0300

    Homework lesson6 final part (finish)

--- test/java/HomeWorkL6P3Test.java
+++ test/java/HomeWorkL6P3Test.java
@@ -10,8 +10,8 @@ public class HomeWorkL6P3Test {
     private static Connection connection;
     private static Statement stmt;
     private static PreparedStatement ps;
-    private static Savepoint sp;
-    HomeWorkL6P3Test homeWorkL6P3Test;
+    //private static Savepoint sp;
+    private static HomeWorkL6P3Test homeWorkL6P3Test;
     private static int markOfTestStudent;
 
     @Before
@@ -39,19 +39,20 @@ public class HomeWorkL6P3Test {
             ps.executeUpdate();
         }
         connection.commit();
-        sp = connection.setSavepoint();
+       // sp = connection.setSavepoint();
 
         ps = connection.prepareStatement("SELECT mark  FROM Students WHERE surname = ?");
         ps.setString(1, "Студент1");
         ResultSet res = ps.executeQuery();
         markOfTestStudent = res.getInt("mark");
+        homeWorkL6P3Test = new HomeWorkL6P3Test();
     }
 
     @Test
     public void testInsert() throws Exception {
         String s = "Иванов55";
         Assert.assertArrayEquals(s.toCharArray(), homeWorkL6P3Test.insert("Иванов", 55));
-        connection.rollback(sp);
+        //connection.rollback(sp);
     }
 
 
@@ -65,7 +66,7 @@ public class HomeWorkL6P3Test {
     public void testUpdate() throws Exception {
         String s = "Студент1" + 99;
         Assert.assertArrayEquals(s.toCharArray(), homeWorkL6P3Test.update("Студент1", 99));
-        connection.rollback(sp);
+        //connection.rollback(sp);
     }
 
     @After
@@ -77,30 +78,28 @@ public class HomeWorkL6P3Test {
         }
     }
 
-    public char[] insert(String surname, int mark) throws Exception {
+    public char[] insert(String fam, int m) throws Exception {
 
         StringBuilder sb = new StringBuilder();
 
         ps = connection.prepareStatement("INSERT INTO Students (surname, mark) VALUES (?, ?)");
-        ps.setString(1, surname);
-        ps.setInt(2, mark);
+        ps.setString(1, fam);
+        ps.setInt(2, m);
         ps.executeUpdate();
-        connection.commit();
 
-        ResultSet res = stmt.executeQuery("SELECT surname, mark  FROM Students WHERE surname = " + surname);
+        ResultSet res = stmt.executeQuery("SELECT surname, mark  FROM Students WHERE surname = '" + fam + "'");
         while (res.next()) {
             sb.append(res.getString("surname"));
             sb.append(res.getInt("mark"));
         }
-
         return sb.toString().toCharArray();
     }
 
-    public char[] select(String surname) throws Exception {
+    public char[] select(String fam) throws Exception {
         StringBuilder sb = new StringBuilder();
 
         ps = connection.prepareStatement("SELECT surname, mark  FROM Students WHERE surname = ?");
-        ps.setString(1, surname);
+        ps.setString(1, fam);
         ResultSet res = ps.executeQuery();
 
         while (res.next()) {
@@ -110,14 +109,15 @@ public class HomeWorkL6P3Test {
         return sb.toString().toCharArray();
     }
 
-    public char[] update(String surname, int mark) throws Exception {
+    public char[] update(String fam, int m) throws Exception {
         StringBuilder sb = new StringBuilder();
 
         ps = connection.prepareStatement("UPDATE Students SET mark = ? WHERE surname = ?");
-        ps.setInt(1, mark);
-        ps.setString(2, surname);
+        ps.setInt(1, m);
+        ps.setString(2, fam);
         ps.executeUpdate();
-        ResultSet res = stmt.executeQuery("SELECT surname, mark  FROM Students WHERE surname = " + surname);
+
+        ResultSet res = stmt.executeQuery("SELECT surname, mark  FROM Students WHERE surname = '" + fam + "'");
         while (res.next()) {
             sb.append(res.getString("surname"));
             sb.append(res.getInt("mark"));
